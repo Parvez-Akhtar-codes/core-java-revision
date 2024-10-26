e4738bb627256f40b6cd2e0a5bf2b6b286ccc03b
Parvez-Akhtar-codes <parvezakhtar310@gmail.com>
2024-10-26 12:26:03 +0530

    commit

--- src/sample.java
+++ src/sample.java
@@ -1,5 +1,18 @@
+class methodsonly{
+    public int m1(){
+        System.out.println("m1");
+        return 2334;
+    }
+}
+
+
+
 public class sample {
     public static void main(String[] args) {
+        methodsonly mo = new methodsonly();
+        if(mo.m1()==2334){
+            System.out.println("true");
 
+        }
     }
 }
